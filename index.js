9a23b257d3dd48b95f16445435200e057b915728
luis-miguel-MI <lmzs1001@gmail.com>
2020-09-13 22:52:09 -0500

    ajustes-intento11

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ app.use(express.json({extend : true}));
 
 
 //Habilitar Cors
-app.use(cors());
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Asignacion de un puerto al servidor
 //Es importante nombrarlo port por temas de despliegue
